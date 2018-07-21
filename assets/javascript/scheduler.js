6bf150fbf20fb6ff5e7d0607ee62f960d9536ace
unknown <38478896+nlee728@users.noreply.github.com>
2018-07-21 18:33:08 -0400

    updated files

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -1,65 +1,65 @@
-// Initialize Firebase
-var config = {
-   apiKey: "AIzaSyCcmkhnfEsQXHAf9FqcWM2FRSXx_6wc1EM",
-   authDomain: "test-907f3.firebaseapp.com",
-   databaseURL: "https://test-907f3.firebaseio.com",
-   projectId: "test-907f3",
-   storageBucket: "test-907f3.appspot.com",
-   messagingSenderId: "659721969084"
- };
+  // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCcmkhnfEsQXHAf9FqcWM2FRSXx_6wc1EM",
+    authDomain: "test-907f3.firebaseapp.com",
+    databaseURL: "https://test-907f3.firebaseio.com",
+    projectId: "test-907f3",
+    storageBucket: "test-907f3.appspot.com",
+    messagingSenderId: "659721969084"
+  };
  firebase.initializeApp(config);
  
- var database=firebase.database();
- var TS = database.ref("Train Schedule/Trains");
- class createEmployee {
-  constructor(name, role, startDate, monthlyRate) {
-  this.name = name;
-  this.role = role;
-  this.startDate = startDate;
-  this.monthlyRate = monthlyRate;
-  }
-}
-
- function calculations() {
-  var name = $("#name-input").val().trim();
-     var role = $("#role-input").val().trim();
-     var startDate = $("#sd-input").val();
-     var monthlyRate = $("#mr-input").val().trim(); 
-  var months = moment().diff(moment(startDate), "months");
-    console.log(months);
-    var totalBilled = (months*monthlyRate);
-    console.log(totalBilled);
- };
-
-   // Capture Button Click
-   $("#add-user").on("click", function(event) {
-     // prevent page from refreshing when form tries to submit itself
+ var database = firebase.database().ref('TrainSchedule/trains');
+ 
+ // Button for adding trains
+ $("#add-train-btn").click(function(event) {
+     // prevent form submisson
      event.preventDefault();
-     // Capture user inputs and store them into variables
-     var name = $("#name-input").val().trim();
-     var role = $("#role-input").val().trim();
-     var startDate = $("#sd-input").val();
-     var monthlyRate = $("#mr-input").val().trim(); 
-     calculations();
-
-      TS.push(new createEmployee(name, role, startDate, monthlyRate, months, totalBilled));
-    });
-TS.on('child_added', function(snap) {
-  const tr = $('<tr>');
-  $('tbody').append(tr);
-  tr.append($("<th>").text(snap.val().name));
-  tr.append($("<th>").text(snap.val().role));
-  tr.append($("<th>").text(snap.val().startDate));
-  tr.append($("<th>").text(snap.val().months));
-  tr.append($("<th>").text(snap.val().monthlyRate));
-  tr.append($("<th>").text(snap.val().totalBilled));    
-});
-
-
-//Months worked and total billed calculations
-var startDate = $("#sd-input").val();
-var monthlyRate = $("#mr-input").val().trim(); 
-
-var months = moment(startDate).diff(moment(), "months");
-
-console.log(months);
+ 
+     // Creates local "temporary" object for holding train data
+     var newTrain = {
+         name: $("#train-name-input").val().trim(),
+         destination: $("#destination-input").val().trim(),
+         frequency: $("#frequency-input").val().trim(),
+     };
+ 
+     // Uploads train data to the database and resets inputs
+     database.push(newTrain);
+     console.log(newTrain);
+     resetInputs();
+ });
+ 
+ // Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+ database.on("child_added", function(childSnapshot) {
+     // Train Info
+     var newTrain = childSnapshot.val();
+     console.log(newTrain);
+     
+     // Calculate the next arrival
+     newTrain.nextArrival = moment().diff(moment.unix(newTrain.frequency, "X"), "minutes");
+     console.log(newTrain.nextArrival);
+     
+     // Calculate the minutes away
+     newTrain.minutesAway = moment().diff(moment.unix(newTrain.frequency, "X"), "minutes");
+     console.log(newTrain.billed);
+ 
+     // Add each train's data into the table
+     $("#train-table > tbody").append(createTrainRow(newTrain));
+ });
+ 
+//  Add new train to table
+ function createTrainRow(train) {
+   var trow = $('<tr>');
+   trow.append('<td>' + train.name + '</td>')
+       .append(`<td>${train.destination}</td>`)
+       .append(`<td>${train.frequency}</td>`)
+       .append(`<td>${train.nextArrival}</td>`)
+       .append(`<td>${train.minutesAway}</td>`);
+   return trow;
+ }
+ 
+//  Reset form fields
+ function resetInputs() { 
+   $("form input:not([submit])").val('');
+   $("#train-name-input").focus();
+ }
