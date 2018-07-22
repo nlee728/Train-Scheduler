83388463b776520442f423fa26214948be351781
unknown <38478896+nlee728@users.noreply.github.com>
2018-07-21 21:33:52 -0400

    updated logic

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -16,7 +16,7 @@
      // prevent form submisson
      event.preventDefault();
  
-     // Creates local "temporary" object for holding train data
+     // Creates local, temporary object for holding train data
      var newTrain = {
          name: $("#train-name-input").val().trim(),
          destination: $("#destination-input").val().trim(),
@@ -34,32 +34,59 @@
      // Train Info
      var newTrain = childSnapshot.val();
      console.log(newTrain);
-     
-     // Calculate the next arrival
-     newTrain.nextArrival = moment().diff(moment.unix(newTrain.frequency, "X"), "minutes");
-     console.log(newTrain.nextArrival);
-     
-     // Calculate the minutes away
-     newTrain.minutesAway = moment().diff(moment.unix(newTrain.frequency, "X"), "minutes");
-     console.log(newTrain.billed);
- 
+
+     // First Time Train Runs (pushed back 1 year to make sure it comes before current time)
+     var firstTimeConverted = moment("04:15", "HH:mm").subtract(1, "years");
+     console.log(firstTimeConverted);
+
+     // Current Time
+     var currentTime = moment();
+     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+     // Difference between the times
+     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+     console.log("DIFFERENCE IN TIME: " + diffTime);
+
+     var frequency = newTrain.frequency;
+     console.log("Frequency: " + frequency);
+
+     // Time apart
+     var tRemainder = diffTime % frequency;
+     console.log("Time remaining: " + tRemainder);
+
+     // Minutes Away
+     var minutesAway = frequency - tRemainder;
+     console.log("MINUTES TILL TRAIN: " + minutesAway);
+
+     // Next Train
+     var nextTrain = moment().add(minutesAway, "minutes");
+     console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
+     var nextArrival = moment(nextTrain).format("hh:mm");
+
      // Add each train's data into the table
      $("#train-table > tbody").append(createTrainRow(newTrain));
  });
  
-//  Add new train to table
+ //  Add new train to table
  function createTrainRow(train) {
-   var trow = $('<tr>');
-   trow.append('<td>' + train.name + '</td>')
-       .append(`<td>${train.destination}</td>`)
-       .append(`<td>${train.frequency}</td>`)
-       .append(`<td>${train.nextArrival}</td>`)
-       .append(`<td>${train.minutesAway}</td>`);
-   return trow;
- }
- 
+    var trow = $('<tr>');
+    trow.append('<td>' + train.name + '</td>')
+        .append(`<td>${train.destination}</td>`)
+        .append(`<td>${train.frequency}</td>`)
+        .append(`<td>${train.nextArrival}</td>`)
+        .append(`<td>${train.minutesAway}</td>`);
+    return trow;
+  }
+
 //  Reset form fields
  function resetInputs() { 
    $("form input:not([submit])").val('');
    $("#train-name-input").focus();
  }
+
+//  Show current time on clock
+function update() {
+  $('#clock').html(moment().format('D. MMMM YYYY H:mm:ss'));
+}
+setInterval(update, 1000);
+
